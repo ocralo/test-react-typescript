10ebc8266b51ddae032bf9f91d73c94f078ee927
ocralo <ocralo@gmail.com>
2020-02-19 14:44:58 -0500

    se crea el componente TextInput

---
+++ src/Components/TextInput/index.tsx
@@ -0,0 +1,10 @@
+//Se importan las librerias necesarias
+import React, { FunctionComponent, useState } from "react";
+
+
+function TextInput() {
+  return <input type="text" name="inputText"/>;
+}
+
+//Se exporta el componente 
+export default TextInput;

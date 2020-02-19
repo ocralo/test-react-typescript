c95ccfc7fd4acad2d9f21969e343efe641de9b5e
ocralo <ocralo@gmail.com>
2020-02-19 16:04:18 -0500

    se crea el componente FocusableTextInput y su hoja de estilo, ademas de anlazarla.

---
+++ src/Components/FocusableTextInput/index.tsx
@@ -0,0 +1,13 @@
+//Se importan las librerias necesarias
+import React, { FunctionComponent, useState } from "react";
+//se importan los estilos
+import "./style.css";
+
+function FocusableTextInput() {
+    return (
+      <div></div>
+  );
+}
+
+//Se exporta el componente
+export default FocusableTextInput;

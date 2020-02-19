769fe8d32b0dc7b7f56f074957540c612a02e5f0
ocralo <ocralo@gmail.com>
2020-02-19 15:14:48 -0500

    se crea la vista de la primera ruta

---
+++ src/views/focusable-input/index.tsx
@@ -0,0 +1,9 @@
+//Se importan las librerias necesarias
+import React, { FunctionComponent, useState } from "react";
+
+function focusableInput() {
+  return <section> </section>;
+}
+
+//Se exporta el componente
+export default focusableInput;

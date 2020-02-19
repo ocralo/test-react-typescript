a56bc670ea2f057d490e164fb1b868482abdc63e
ocralo <ocralo@gmail.com>
2020-02-19 15:00:59 -0500

    se agrega el css

--- src/Components/Spinner/index.tsx
+++ src/Components/Spinner/index.tsx
@@ -1,5 +1,6 @@
 //Se importan las librerias necesarias
 import React, { FunctionComponent, useState } from "react";
+import "./style.css";
 
 //funcion para crear un spinner en las pantallas de carga
 function Spinner() {

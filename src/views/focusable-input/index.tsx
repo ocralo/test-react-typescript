4a8ae5797482512786b92540fda014bbd0f3dca4
ocralo <ocralo@gmail.com>
2020-02-19 15:57:53 -0500

    se corrige el html, se enlaza el componente textinput y se enlaza la hoja de estilos

--- src/views/focusable-input/index.tsx
+++ src/views/focusable-input/index.tsx
@@ -1,8 +1,18 @@
 //Se importan las librerias necesarias
 import React, { FunctionComponent, useState } from "react";
+//se importan los estilos
+import "./style.css";
+//Se importan los componentes
+import TextInput from "../../Components/TextInput";
 
 function focusableInput() {
-  return <section> </section>;
+  return (
+    <section className="viewTest1">
+      <form>
+        <TextInput />
+      </form>
+    </section>
+  );
 }
 
 //Se exporta el componente

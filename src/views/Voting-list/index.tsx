7ae85c5cea079144f6503e81dab4daa2a3c26292
ocralo <ocralo@gmail.com>
2020-02-19 22:51:15 -0500

    se crea la vista voting-list y sus estilos

---
+++ src/views/Voting-list/index.tsx
@@ -0,0 +1,14 @@
+//Se importan las librerias necesarias
+import React from "react";
+import "./style.css";
+
+function VotingList() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+//Se exporta el componente
+export default VotingList;

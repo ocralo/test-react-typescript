0eeb16a65cbb50cae9d628941f6f448416788d1f
ocralo <ocralo@gmail.com>
2020-02-20 14:37:35 -0500

    se corriguen la estructura de los inputs y se crea la funcion, para los mensajes de error

--- src/views/Register-form/index.tsx
+++ src/views/Register-form/index.tsx
@@ -9,7 +9,11 @@ function RegisterForm() {
     email: "",
     phoneNumber: 0
   });
-  const InputUserRef = useRef(null);
+  const [phoneInit, setPhoneInit] = useState<number>(300);
+
+  const InputUserRef = useRef<HTMLInputElement>(null);
+  const InputUserPhone = useRef<HTMLInputElement>(null);
+  const InputUserPhoneInit = useRef<HTMLInputElement>(null);
 
   /* Funcion para cambiar los datos del usuario cada vez que hay un cambio en el input
      las variables de entrada es in HTMLInputElement
@@ -20,14 +24,12 @@ function RegisterForm() {
         ...userRegister,
         userName: inputForm.value
       });
-    }
-    else if (inputForm.name === "email") {
+    } else if (inputForm.name === "email") {
       setUserRegister({
         ...userRegister,
         email: inputForm.value
       });
-    }
-    else {
+    } else {
       setUserRegister({
         ...userRegister,
         phoneNumber: Number(inputForm.value)
@@ -35,7 +37,29 @@ function RegisterForm() {
     }
   }
 
-  useEffect(() => {}, [userRegister]);
+  /* Funcion, para detectar los errores en la inserción de datos en el formulario */
+  function checkInput() {
+    //deteccion de error en el input de Username
+    if (InputUserRef.current?.validity.patternMismatch) {
+      InputUserRef.current.setCustomValidity(
+        "El nombre de usuario debe tener al menos 4 caracteres y maximo 20 caracteres, sin espacios. "
+      );
+    } else {
+      InputUserRef.current?.setCustomValidity("");
+    }
+    //deteccion de error en el input de phone
+    if (InputUserPhone.current?.validity.patternMismatch) {
+      InputUserPhone.current.setCustomValidity(
+        "El telefono del usuario debe tener al menos un numero entre 300 y 320 "
+      );
+    } else {
+      InputUserPhone.current?.setCustomValidity("");
+    }
+  }
+
+  useEffect(() => {
+    checkInput();
+  }, [userRegister]);
   return (
     <div className="container-form">
       <h1 className="form-title">Formulario de Registro</h1>
@@ -58,19 +82,36 @@ function RegisterForm() {
         </label>
         <label className="label-group" htmlFor="phone">
           <span>Telefono celular</span>
-          <input
-            className="input-form"
-            type="number"
-            name="phoneNumber"
-            id="phone"
-            placeholder="300 3434 3434"
-            min="300"
-            max="320"
-            required
-            onChange={e => {
-              changeDataUser(e.target);
-            }}
-          />
+          <div>
+            <input
+              className="input-form"
+              type="number"
+              name="phoneNumber"
+              id="phone"
+              placeholder="300"
+              value={phoneInit}
+              min="300"
+              max="320"
+              ref={InputUserPhoneInit}
+              required
+              onChange={e => {
+                setPhoneInit(Number(e.target.value))
+              }}
+            />
+            <input
+              className="input-form"
+              type="number"
+              name="phoneNumber2"
+              id="phone2"
+              placeholder="3434 3434"
+              value={userRegister.phoneNumber}
+              ref={InputUserPhone}
+              required
+              onChange={e => {
+                changeDataUser(e.target);
+              }}
+            />
+          </div>
         </label>
         <label className="label-group" htmlFor="mail">
           <span>Correo electronico</span>

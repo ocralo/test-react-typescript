0b739a143567bc595adb41128875446d6adf7cb4
ocralo <ocralo@gmail.com>
2020-02-20 00:21:10 -0500

    se crea la estructura de la card de candidatos

--- src/Components/CardCandidate/index.tsx
+++ src/Components/CardCandidate/index.tsx
@@ -1,11 +1,26 @@
 //Se importan las librerias necesarias
 import React from "react";
-import { RouteComponentProps } from "react-router-dom";
 
 import "./style.css";
 
 function CardCandidate() {
-    return <div></div>;
+  return (
+    <div className="card-candidate">
+      <div className="col-card-left">
+        <h2>Candidato:</h2>
+        <p>nombre apellido1 apellido2</p>
+        <p>edad</p>
+        <p>slogan</p>
+      </div>
+      <div className="col-card-right">
+        <h2>Votos numero</h2>
+        <div>
+          <button>+</button>
+          <button>-</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 //Se exporta el componente

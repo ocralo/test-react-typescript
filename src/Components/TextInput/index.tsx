c22a737b972ce27ee99f7de9889aa1be67028d5e
ocralo <ocralo@gmail.com>
2020-02-19 20:13:53 -0500

    se reciben los datos del props y se acomoda para cambiar la variable cuando este focus o no

--- src/Components/TextInput/index.tsx
+++ src/Components/TextInput/index.tsx
@@ -1,13 +1,23 @@
 //Se importan las librerias necesarias
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useState, useRef } from "react";
 import "./style.css";
 
 function TextInput(props: IProps) {
-  return <input className="inputFocusable" type="text" placeholder="..." />;
+  return (
+    <input
+      className="inputFocusable"
+      type="text"
+      placeholder="..."
+      onFocus={e=>{props.propSetFocused(true)}}
+      onBlur={e=>{props.propSetFocused(false)}}
+    />
+  );
 }
 
-interface IProps {
+interface IProps{
   focused?: boolean;
+  propSetFocused: (value: boolean | ((prevVar: boolean) => boolean)) => void;
+  /* propSetFocused: (value: boolean | ((prevVar: boolean) => boolean)) => void; */
 }
 //Se exporta el componente
 export default TextInput;

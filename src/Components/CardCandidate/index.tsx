1636bea679b576bf52b171253e163a19e2aa241e
ocralo <ocralo@gmail.com>
2020-02-19 23:39:05 -0500

    se crea el componente que mostrara los datos de los candidatos

---
+++ src/Components/CardCandidate/index.tsx
@@ -0,0 +1,12 @@
+//Se importan las librerias necesarias
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+
+import "./style.css";
+
+function CardCandidate() {
+    return <div></div>;
+}
+
+//Se exporta el componente
+export default CardCandidate;

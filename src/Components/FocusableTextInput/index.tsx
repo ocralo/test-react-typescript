2af6ed43f72455dadbbba7a173f1bbb75df3a2d9
ocralo <ocralo@gmail.com>
2020-02-19 21:12:16 -0500

    correccion de recarga de pagina por el submit y configuracion de como se ve el texto para saber si esta focus o no el input

--- src/Components/FocusableTextInput/index.tsx
+++ src/Components/FocusableTextInput/index.tsx
@@ -5,15 +5,24 @@ import "./style.css";
 //Se importan los componentes necesarios
 import TextInput from "../../Components/TextInput";
 
-
 function FocusableTextInput() {
   const [focused, setFocused] = useState<boolean>(false);
-    return (
-      <form>
-        <TextInput propSetFocused={setFocused} />
-        <h1>{`${focused}`}</h1>
-      </form>
-    );
+
+  return (
+    <form
+      className="formFocusableTextInput"
+      onSubmit={e => {
+        e.preventDefault();
+      }}
+    >
+      <TextInput propSetFocused={setFocused} />
+      {focused !== true ? (
+        <h1 className="stateInput reedText">No esta focus el input</h1>
+      ) : (
+        <h1 className="stateInput greenText">Esta focus el input</h1>
+      )}
+    </form>
+  );
 }
 
 //Se exporta el componente

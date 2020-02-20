f64a8669a8b61862821f58682ed482ba37bd418e
ocralo <ocralo@gmail.com>
2020-02-19 23:33:05 -0500

    se configura para recibir el umero de candidatos mediante la url

--- src/views/Voting-list/index.tsx
+++ src/views/Voting-list/index.tsx
@@ -1,13 +1,25 @@
 //Se importan las librerias necesarias
 import React from "react";
+import {RouteComponentProps} from "react-router-dom";
+
 import "./style.css";
 
-function VotingList() {
-  return (
-    <div>
-      
-    </div>
-  );
+function VotingList(props: ComponentProps) {
+    return <div>{props.match.params.candidates}</div>;
+}
+
+interface ICandidates{
+    firstname: string,
+    lastname: string,
+    age: number,
+    slogan: string,
+    votes: number
+}
+interface RouteInfo {
+    candidates: string;
+}
+
+interface ComponentProps extends RouteComponentProps<RouteInfo> {
 }
 
 //Se exporta el componente

2ccfe614e23c309e42ecb9b282a1d66a8530f9f4
ocralo <ocralo@gmail.com>
2020-02-19 20:46:42 -0500

    se crea la vista 404 y se anlaza su hoja de estilos, ademas de crear la hoja de estilos

---
+++ src/views/Page404/index.tsx
@@ -0,0 +1,26 @@
+//Se importan las librerias necesarias
+import React, { useState, useRef } from "react";
+import "./style.css";
+
+function Page404() {
+  return (
+    <div className="container">
+      <div className="banner">
+        <p className="text-404">Error 404</p>
+      </div>
+      <div>
+        <button className="button">Regresar</button>
+      </div>
+    </div>
+  );
+}
+
+//Se exporta el componente
+export default Page404;
+
+/* <div class="banner">
+        <p>404</p>
+    </div>
+    <div>
+        <button onclick="goBack()">GO BACK</button>
+    </div> */
